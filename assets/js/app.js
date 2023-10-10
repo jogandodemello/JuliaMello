8bf4e04972d942d803f67818c55f7fbddf153bae
Matt Nejedlo <144960173+Mattnej21@users.noreply.github.com>
2023-10-10 18:06:28 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>\
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="reference.html">References</a></li>
-  <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/john_doe.pdf" download="assets/docs/Matthew_NejedloAlpha.pdf">Resumé/CV</a></li>
+  <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/Matthew_NejedloAlpha.pdf" download="assets/docs/Matthew_NejedloAlpha.pdf">Resumé/CV</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
